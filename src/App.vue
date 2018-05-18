02920641cebcf0cab11b64ef701d7ab43ab60de4
Igor Oliinyk <igor.oleynikdp@gmail.com>
2018-05-18 19:05:19 +0300

    Добавили кнопку и обработчик события которые будут менять имя по клику Но имя мы хотим вывести в дочернем компоненте

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div class="bg-white mx-auto mt-8 p-8 max-w-m shadow-lg rounded-lg overflow-hidden">
       <h1>The User Component</h1>
       <p>I'm an awesome user!</p>
+      <button @click="changeName" class="text-xl mt-4 font-hairline rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple hover:bg-purple hover:text-white">Change my name!</button>
       <hr class="border">
       <div class="flex mb-4">
         <div class="w-1/2 mr-4">
@@ -21,6 +22,16 @@
 import UserDetails from './components/UserDetails.vue'
 import EditUser from './components/EditUser.vue'
 export default {
+  data() {
+    return {
+      name: 'Igor'
+    }
+  },
+  methods: {
+    changeName() {
+      this.name = 'Anna'
+    }
+  },
   name: 'app',
   components: {
     UserDetails,
